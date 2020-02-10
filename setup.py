bd455076b8e8c24e7f9f654bc55063f8d0402a7b
Yash Aggarwal <yash@ip-192-168-4-216.ap-southeast-1.compute.internal>
2020-02-10 15:41:24 +0530

    Changing setup.py file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    name="example-pkg-yash2406", # Replace with your own username
     version="0.0.1",
     author="Yash Aggarwal",
     author_email="yashaggarwal1112@gmail.com",
